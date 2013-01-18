df8dc2ed177a166ae04cf80bbf103d9f05057bc9
Manoj <pmontu@gmail.com>
2013-01-18 22:35:50 +0530

    create patient form
    wpf tutorial grid and stackpanel fixed heights inspired
    
    control alignment margin -
    http://social.msdn.microsoft.com/forums/en-US/wpf/thread/e121bb8f-c4ad-4e8a-b30d-c40047dd2a1b
    sliderpanel - http://msdn.microsoft.com/en-us/vstudio/cc707833.aspx
    
    multiline textbox -
    http://stackoverflow.com/questions/2650144/multiline-for-wpf-textbox
    modal windows -
    http://stackoverflow.com/questions/499294/how-do-make-modal-dialog-in-wpf
    tabitem header bold -
    http://social.msdn.microsoft.com/Forums/en/wpf/thread/486f196b-1ad3-46c9-9ed7-4993078f01a3
    
    output - http://s1.postimage.org/pn8od48dr/Untitled.png

--- PatientRecordsWPF2/PatientDetails.xaml.cs
+++ PatientRecordsWPF2/PatientDetails.xaml.cs
@@ -10,31 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace PatientRecordsWPF2
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for PatientDetails.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class PatientDetails : Window
     {
-        public MainWindow()
+        public PatientDetails()
         {
             InitializeComponent();
-        }
-
-        private void btnViewPatientDetails_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-        private void btnAddNewPatient_Click(object sender, RoutedEventArgs e)
-        {
-            var pd = new PatientDetails();
-            pd.Owner = this;
-            pd.ShowDialog();
-
         }
     }
 }
