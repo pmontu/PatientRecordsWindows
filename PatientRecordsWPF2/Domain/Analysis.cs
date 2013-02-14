c01f0f4a86b32ef89217f30e6c085163ccacb812
Manoj <pmontu@gmail.com>
2013-02-14 11:08:48 +0530

    Analysis - Entity & Model
    trying new encapsulation aproach to reduce the mess on one page

---
+++ PatientRecordsWPF2/Domain/Analysis.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatientRecordsWPF2.Domain
+{
+    class Analysis
+    {
+        public List<string> Symptoms;
+        public DateTime From;
+        public DateTime To;
+        public Analysis()
+        {
+            Symptoms = new List<string>();
+        }
+    }
+}
